fdd0c92ec937b1e509cca902978e98c51dff321f
HalakII <135172329+HalakII@users.noreply.github.com>
2024-05-05 18:14:22 +0000

    Deploying to gh-pages from @ HalakII/campers_for_rent@5edfbf1221750d3937eb5e5025e62a0cfc6faad0 🚀

---
+++ static/js/987.432cc6bb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcampers_for_rent=self.webpackChunkcampers_for_rent||[]).push([[987],{4413:function(r,e,n){n.r(e),n.d(e,{default:function(){return i}});var c=n(4420),t=n(2791),s=n(3666),u=n(945),f=n(1917),a=n(3329);function i(){var r=(0,c.v9)(u.Tc),e=(0,c.I0)();return(0,t.useEffect)((function(){e((0,s.cU)())}),[e]),(0,a.jsx)(a.Fragment,{children:(0,a.jsx)(f.Z,{cards:r,isFavorites:!0})})}}}]);
+//# sourceMappingURL=987.432cc6bb.chunk.js.map
